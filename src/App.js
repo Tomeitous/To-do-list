11738186bb1596ff2be071a9087b9a15ba4ce575
Pauli Hemmi <lolled.mr@gmail.com>
2022-10-13 11:36:09 +0300

    cleaned code

--- src/App.js
+++ src/App.js
@@ -15,37 +15,23 @@ function App() {
   };
   return (
     <div className="App">
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            To Do List
-          </Typography>
-          <Tabs
-            textColor="secondary"
-            indicatorColor="secondary"
-            value={tabIndex}
-            onChange={handleTabChange}
-            centered
-          >
-            <Tab label="Home" />
-            <Tab label="To do" />
-          </Tabs>
-        </AppBar>
-      </Box>
-      <Box sx={{ margin: 2 }}>
-        {tabIndex === 0 && (
-          <Box>
-            <Typography>Ostin nuget</Typography>
-          </Box>
-        )}
-        {tabIndex === 1 && (
-          <Box>
-            <Typography>
-              <Todolist></Todolist>
-            </Typography>
-          </Box>
-        )}
-      </Box>
+      <AppBar position="static">
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          To Do List
+        </Typography>
+        <Tabs
+          textColor="secondary"
+          indicatorColor="secondary"
+          value={tabIndex}
+          onChange={handleTabChange}
+          centered
+        >
+          <Tab label="Home" />
+          <Tab label="To do" />
+        </Tabs>
+      </AppBar>
+      {tabIndex === 0 && <Typography>Ostin nuget</Typography>}
+      {tabIndex === 1 && <Todolist></Todolist>}
     </div>
   );
 }
